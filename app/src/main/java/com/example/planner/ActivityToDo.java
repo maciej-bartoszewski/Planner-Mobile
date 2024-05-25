a1c7df11abdbbaf3b3680cccfc179e9e47ad94cf
Łukasz <245968@edu.p.lodz.pl>
2024-05-25 18:19:50 +0200

    Dodanie Funkcjonalnosci dodawania notatek

--- app/src/main/java/com/example/planner/ActivityToDo.java
+++ app/src/main/java/com/example/planner/ActivityToDo.java
@@ -2,22 +2,35 @@ package com.example.planner;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class ActivityToDo extends AppCompatActivity {
 
@@ -41,6 +54,8 @@ public class ActivityToDo extends AppCompatActivity {
         initializeViews();
         setTxtDate();
         setGreeting();
+        initializeaddNoteButton();
+
     }
 
     private void initializeViews() {
@@ -51,6 +66,9 @@ public class ActivityToDo extends AppCompatActivity {
         addButton = findViewById(R.id.addButton);
         goodMorning = findViewById(R.id.goodMorning);
         textData = findViewById(R.id.textData);
+
+        MainNavigationView mainNavigationView = new MainNavigationView();
+        mainNavigationView.initializeNavigationView(menuButton, drawerLayout, navigationView, user, ActivityToDo.this);
     }
     private void setGreeting() {
         Calendar calendar = Calendar.getInstance();
@@ -88,4 +106,82 @@ public class ActivityToDo extends AppCompatActivity {
 
         textData.setText(formattedDate);
     }
+    private void initializeaddNoteButton() {
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNotePopUp = new Dialog(ActivityToDo.this);
+                addNotePopUp.setContentView(R.layout.activity_todo_pop_up);
+                addNotePopUp.show();
+
+                addNotePopUp.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                addNotePopUp.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                initializeSendaddNoteButton(addNotePopUp);
+            }
+        });
+    }
+    public static boolean isValidDate(String dateString) {
+        if (dateString == null || !dateString.matches("\\d{2}/\\d{2}/\\d{4}")) {
+            return false;
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy", Locale.getDefault());
+        sdf.setLenient(false);
+        try {
+            Date date = sdf.parse(dateString);
+            return date != null;
+        } catch (ParseException e) {
+            return false;
+        }
+    }
+    private void initializeSendaddNoteButton(Dialog addNotePopUp) {
+        Button addNote = addNotePopUp.findViewById(R.id.confirmButton);
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText title = addNotePopUp.findViewById(R.id.editTextTitle);
+                EditText date = addNotePopUp.findViewById(R.id.editTextdate);
+                TextView invalidDate = addNotePopUp.findViewById(R.id.invalidDate);
+                EditText description = addNotePopUp.findViewById(R.id.EditTextDescription);
+
+                if (title.getText().toString().isEmpty() || description.getText().toString().isEmpty()) {
+                    invalidDate.setText("Brak tytułu/opisu");
+                    invalidDate.setVisibility(View.VISIBLE);
+                } else {
+                    invalidDate.setVisibility(View.INVISIBLE);
+
+                    if (isValidDate(date.getText().toString())) {
+                        Map<String, Object> note = new HashMap<>();
+                        //notatka
+                        note.put("title", title.getText().toString());
+                        note.put("date", date.getText().toString());
+                        note.put("description", description.getText().toString());
+                        note.put("email", user.getEmail().toString());
+
+                        db.collection("toDoList").add(note).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(ActivityToDo.this, "Dodano notatke", Toast.LENGTH_SHORT).show();
+                                containerLayout.removeAllViews();
+//                                displayNotes();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(ActivityToDo.this, "blad dodawania notatki", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        if (addNotePopUp.isShowing()) {
+                            addNotePopUp.dismiss();
+                        }
+                    } else {
+                        invalidDate.setText("Niepoprawna data (Format DD/MM/RR)");
+                        invalidDate.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+        });
+
+    }
+
 }
