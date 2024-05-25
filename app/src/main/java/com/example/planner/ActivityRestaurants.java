a24a6698d1b8b3851346e8508c9aeb6e9083109b
Mateusz <245964@edu.p.lodz.pl>
2024-05-25 21:07:10 +0200

    Dodanie inicjalizacji menu

--- app/src/main/java/com/example/planner/ActivityRestaurants.java
+++ app/src/main/java/com/example/planner/ActivityRestaurants.java
@@ -1,11 +1,14 @@
 package com.example.planner;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.google.android.material.navigation.NavigationView;
 
 public class ActivityRestaurants extends AppCompatActivity {
@@ -14,10 +17,14 @@ public class ActivityRestaurants extends AppCompatActivity {
     LinearLayout restaurantContainerLayout;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        user = intent.getParcelableExtra("User");
+
         setContentView(R.layout.activity_restaurant_view);
         initializeViews();
     }
@@ -28,5 +35,8 @@ public class ActivityRestaurants extends AppCompatActivity {
         navigationView = findViewById(R.id.navigationView);
         drawerLayout = findViewById(R.id.drawerLayout);
         menuButton = findViewById(R.id.menuButton);
+
+        MainNavigationView mainNavigationView = new MainNavigationView();
+        mainNavigationView.initializeNavigationView(menuButton, drawerLayout, navigationView, user, ActivityRestaurants.this);
     }
 }
